68ec356a1c71cc4678a8fffa861bc183f2381ff0
Tomoki Sakamoto <tsak0007@student.monash.edu>
2025-08-07 03:02:42 +1000

    Initial commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="my-name">
+    <p @click="changeName">Hello、{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  data() {
+    return {
+      name: 'Tomoki'
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = prompt('Enter your Name', this.name) || this.name
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-name {
+  font-size: 1.2rem;
+  cursor: pointer;
+  color: #333;
+}
+.my-name:hover {
+  color: #007bff;
+}
+</style>
